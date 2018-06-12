21f52764ce9353b7fdcb5097e4cf3a899abf1d5b
CatchZeng <891793848@qq.com>
2018-06-12 16:48:09 +0800

    :sparkles: Add demo project.
    Add iOS demo project.

---
+++ LearningOpenCV/LearningOpenCV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningOpenCV
+//
+//  Created by CatchZeng on 2018/6/12.
+//  Copyright © 2018年 catch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LearningOpenCV
+
 Learning OpenCV with iOS.
+
+[OpenCV官方主页](https://opencv.org)
+
+[OpenCV github主页](https://github.com/opencv/opencv)
